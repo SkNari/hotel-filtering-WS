b92bdeb0e1f7bc424b3c732366ffb70312ad1aed
luaugusto@laposte.net <luaugusto@laposte.net>
2020-11-25 11:41:06 +0100

    test

--- hotels_check/auth.py
+++ hotels_check/auth.py
@@ -1,7 +1,7 @@
 import requests
 
 def check_token(token):
-    r = requests.post('http://localhost:8080/api/checkToken', data = {'token': token}).json()
+    r = requests.post('http://localhost:8083/api/checkToken', data = {'token': token}).json()
     if 'data' in r:
         return True, r['username']
     else:
